41530760f7ef0a5d2b64e6eb88d89e6ab77245e9
Pankaj Bhandari <Pankaj.Bhandari@aristocrat.com>
2021-09-13 16:51:46 -0400

    Implemented secrets

--- Catalog/Configurations/MongoDbSettings.cs
+++ Catalog/Configurations/MongoDbSettings.cs
@@ -11,11 +11,15 @@ namespace Catalog.Configurations
 
         public int Port { get; set; }
 
+        public string User { get; set; }
+
+        public string Password { get; set; }
+
         public string ConnectionString
         {
             get
             {
-                return $"mongodb://{Host}:{Port}";
+                return $"mongodb://{User}:{Password}@{Host}:{Port}";
             }
         }
     }
